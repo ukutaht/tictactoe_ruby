c68f2dc491c840b17f3e2de4b31a8b33e6cc8859
heruku <uku.taht@gmail.com>
2014-06-24 09:32:38 +0100

    fix board tests

--- spec/qt_gui/board_widget_spec.rb
+++ spec/qt_gui/board_widget_spec.rb
@@ -74,17 +74,20 @@ describe TicTacToe::Qt_GUI::BoardWidget do
     end
 
     it 'plays through the whole when comp vs comp' do
+      allow(window).to receive(:display).and_return(double.as_null_object)
       game.computer_vs_computer
       9.times { emit(window.timer.timeout()) }
       expect(game).to be_over
     end
 
     it 'does not try to make a comp move afte game is over' do
+      allow(window).to receive(:display).and_return(double.as_null_object)
       game.computer_vs_computer
       expect { 10.times { emit(window.timer.timeout()) }}.to_not raise_error
     end
 
     it 'does not try make a human move after game is over' do
+      allow(window).to receive(:display).and_return(double.as_null_object)
       game.human_vs_human
       [0, 1, 4, 5, 8].each do |i|
         game.play_next_move(i)
@@ -95,6 +98,7 @@ describe TicTacToe::Qt_GUI::BoardWidget do
     end
 
     it 'can play through the whole game through gui' do
+      allow(window).to receive(:display).and_return(double.as_null_object)
       game.human_vs_human
 
       [0, 1, 4, 5, 8].each do |i|
